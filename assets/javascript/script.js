55d6cb4f7b80929888ea66c9692138c706b71912
Josh Massa <joshmassa@gmail.com>
2024-01-09 18:13:58 -0500

    Added a function with variables for generatePassword. Adjusted margin on .wrapper

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var uppercase = confirm("Would you like your password to contain uppercase letters? Please click 'OK' for Yes or 'Cancel' for No.");
+  var lowercase = confirm("Would you like your password to contain lowercase letters? Please click 'OK' for Yes or 'Cancel' for No.");
+  var symbols = confirm("Would you like your password to contain special characters? Please click 'OK' for Yes or 'Cancel' for No.");
+  var numbers = confirm("Would you like your password to contain numbers? Please click 'OK' for Yes or 'Cancel' for No.");
+  var keyLength = prompt("Password must be between 8 and 32 characters, please enter your desired length and click 'OK' to continue");
+  var uppercaseAllowed = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercaseAllowed = "abcdefghijklmnopqrstuvwxyz";
+  var scAllowed = "@%+\/'!#$^?:,(){}[]~-_."
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
